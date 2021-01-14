bb21c0197153341b71cafc0e28ffdffb4c79df2c
ivasik <51726661+Blackblackofficial@users.noreply.github.com>
2021-01-14 21:39:59 +0300

    Add search files

--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -2,10 +2,12 @@ package apiserver
 
 type Config struct {
 	StateAddr string `toml:"state_addr"`
+	LogLevel string `toml:"log_level"`
 }
 
 func NewConfig() *Config {
 	return &Config{
 		StateAddr: ":8080",
+		LogLevel: "debug",
 	}
 }
