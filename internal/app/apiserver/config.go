35baf41c73067bed88150e5db6c0a2e662a69989
ivasik <51726661+Blackblackofficial@users.noreply.github.com>
2021-01-25 23:59:44 +0300

    Add Db

--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -1,13 +1,17 @@
 package apiserver
 
+import "awesomeProject/internal/store"
+
 type Config struct {
 	StateAddr string `toml:"state_addr"`
 	LogLevel string `toml:"log_level"`
+	Store *store.Config
 }
 
 func NewConfig() *Config {
 	return &Config{
 		StateAddr: ":8080",
 		LogLevel: "debug",
+		Store: store.NewConfig(),
 	}
 }
